6e6dba9640c0f51d895c05545358bbaa4574f5cd
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-07-12 19:35:50 +0300

    updated Article.js and Comment.js

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Article() {
-  return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
-}
+    class Article extends React.Component {
+      render() {
+        return <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+      }
+    }
+
 
 export default Article;
